d90f5143c827945497c21ec3d0fc8e47636e2776
Ismael Junior <ismaeljcjunior@gmail.com>
2023-01-20 13:33:06 -0300

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
+import { CardBell } from './components/CardBell';
+import { PageBells } from './pages/PageBells';
 
 export const App = () => {
 
   return (
     <div className="text-green-500">
-      teste
+      <CardBell />
     </div>
   )
 }
